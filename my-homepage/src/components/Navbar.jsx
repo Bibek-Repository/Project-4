3a5d99a249e408e36be5809300aa0c0c60cb6d86
[Bibek Baiju] <{bibekbaiju18@gmail.com]>
2026-07-01 01:12:57 +0545

    navbar fixation

--- my-homepage/src/components/Navbar.jsx
+++ my-homepage/src/components/Navbar.jsx
@@ -1,31 +1,95 @@
-
-import '../index.css';
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 import logo from "../assets/ai_solutions_logo.png";
 
-
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="logo">
-        <Link to="/" className="logo">
-          <img src={logo} alt="AI Solutions Logo" className="logo-img" />
-        </Link>
-      </div>
-      
-      
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/About">About Us</Link></li>
-        <li><Link to="/Reviews">Customer Ratings & Feedbacks</Link></li>
-        <li><Link to="/Contact">Contact Us</Link></li>
-        <li><Link to="/GalleryBlog">Photo Galleries and Blogs</Link></li>
-        <li><Link to="/Events">Events Timeline</Link></li>
-        <li><Link to="/FinalServices">Provided Software Solutions</Link></li>
-        <li><Link to="/admin/login">Admin</Link></li>
-      </ul>
-    </nav>
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        {/* Logo */}
+        <Link to="/" className="navbar-logo" onClick={closeMenu}>
+          <img
+            src={logo}
+            alt="AI Solutions Logo"
+            className="logo-img"
+          />
+        </Link>
+
+        {/* Hamburger */}
+        <div
+          className={menuOpen ? "menu-icon active" : "menu-icon"}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation Links */}
+        <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/About" onClick={closeMenu}>
+              About Us
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Reviews" onClick={closeMenu}>
+              Customer Ratings & Feedbacks
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Contact" onClick={closeMenu}>
+              Contact Us
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/GalleryBlog" onClick={closeMenu}>
+              Photo Galleries and Blogs
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Events" onClick={closeMenu}>
+              Events Timeline
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/FinalServices" onClick={closeMenu}>
+              Provided Software Solutions
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/admin/login" onClick={closeMenu}>
+              Admin
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Background Overlay */}
+      <div
+        className={menuOpen ? "menu-overlay active" : "menu-overlay"}
+        onClick={closeMenu}
+      ></div>
+    </>
   );
 }
 
